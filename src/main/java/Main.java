cbc63af1b5094104ce4ccf087e1ae958f3dd4dac
Adrian Wieczorek <adrian.wieczorek2001@proton.me>
2026-07-16 14:42:54 +0200

    feat: added new method printSummary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main {
@@ -26,6 +27,45 @@ public class Main {
             income.put(category, newCategory);
             newCategory.add(proceeds);
         }
+    }
+
+    public void printSummary(HashMap<String, ArrayList<Double>> income,
+                             HashMap<String, ArrayList<Double>> expenses) {
+
+        double totalIncome = 0;
+        double totalExpenses = 0;
+
+        for (Map.Entry<String, ArrayList<Double>> entry : income.entrySet()) {
+
+            double categorySum = 0;
+
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                totalIncome += entry.getValue().get(i);
+                categorySum += entry.getValue().get(i);
+            }
+
+            System.out.println(entry.getKey() + " whole income " + categorySum);
+
+        }
+
+        for (Map.Entry<String, ArrayList<Double>> entry : expenses.entrySet()) {
+
+            double categorySum = 0;
+
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                totalExpenses += entry.getValue().get(i);
+                categorySum += entry.getValue().get(i);
+            }
+
+            System.out.println(entry.getKey() + " whole expenses " + categorySum);
+
+        }
+
+        double total = totalIncome - totalExpenses;
+
+        System.out.println("Total expenses: " + totalExpenses
+                + "\nTotal income: " + totalIncome
+                + "\nSum: " + total);
 
     }
 
