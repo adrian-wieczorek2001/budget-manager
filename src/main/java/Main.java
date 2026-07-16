704a8593342e3f9ebe5b9f5fe61883b1c59447b0
Adrian Wieczorek <adrian.wieczorek2001@proton.me>
2026-07-16 14:19:42 +0200

    feat: add new addIncome method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-
 public class Main {
 
     public void addExpenses(HashMap<String, ArrayList<Double>> expenses,
@@ -14,6 +13,18 @@ public class Main {
             ArrayList<Double> newCategory = new ArrayList<>();
             expenses.put(category, newCategory);
             newCategory.add(cost);
+        }
+    }
+
+    public void addIncome(HashMap<String, ArrayList<Double>> income,
+                          String category, Double proceeds) {
+
+        if (income.containsKey(category)) {
+            income.get(category).add(proceeds);
+        } else {
+            ArrayList<Double> newCategory = new ArrayList<>();
+            income.put(category, newCategory);
+            newCategory.add(proceeds);
         }
 
     }
